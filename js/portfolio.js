22c5e11970650f63e1fde479cc583385ed42f52a
piotrjacak <piotrjac23@gmail.com>
2022-10-20 22:46:51 +0200

    initial commit

---
+++ js/portfolio.js
@@ -0,0 +1,102 @@
+const targetDivSuzuka = document.querySelector('.suzukaVideo');
+const suzuka_btn = document.querySelector('.suzuka');
+
+targetDivSuzuka.style.display = "none";
+
+suzuka_btn.onclick = function () {
+    if (targetDivSuzuka.style.display !== "none") {
+      targetDivSuzuka.style.display = "none";
+    } else {
+      targetDivSuzuka.style.display = "block";
+      targetDivSuzuka.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"}); 
+    }};
+
+
+
+const targetDivTatra = document.querySelector('.tatraMountainsVideo');
+const tatra_btn = document.querySelector('.tatraMountains');
+
+targetDivTatra.style.display = "none";
+
+tatra_btn.onclick = function () {
+    if (targetDivTatra.style.display !== "none") {
+      targetDivTatra.style.display = "none";
+    } else {
+      targetDivTatra.style.display = "block";
+      targetDivTatra.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"}); 
+    }};
+
+
+
+const targetDivRome = document.querySelector('.romeVideo');
+const rome_btn = document.querySelector('.rome');
+
+targetDivRome.style.display = "none";
+
+rome_btn.onclick = function () {
+    if (targetDivRome.style.display !== "none") {
+      targetDivRome.style.display = "none";
+    } else {
+      targetDivRome.style.display = "block";
+      targetDivRome.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"}); 
+    }};
+
+
+
+const targetDivCopenhagen = document.querySelector('.copenhagenVideo');
+const copenhagen_btn = document.querySelector('.copenhagen');
+
+targetDivCopenhagen.style.display = "none";
+
+copenhagen_btn.onclick = function () {
+    if (targetDivCopenhagen.style.display !== "none") {
+      targetDivCopenhagen.style.display = "none";
+    } else {
+      targetDivCopenhagen.style.display = "block";
+      targetDivCopenhagen.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"}); 
+    }};
+
+
+
+const targetDivMadness = document.querySelector('.madnessVideo');
+const madness_btn = document.querySelector('.madness');
+
+targetDivMadness.style.display = "none";
+
+madness_btn.onclick = function () {
+    if (targetDivMadness.style.display !== "none") {
+      targetDivMadness.style.display = "none";
+    } else {
+      targetDivMadness.style.display = "block";
+      targetDivMadness.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"}); 
+    }};
+
+
+
+const targetDivPizza = document.querySelector('.pizzaVideo');
+const pizza_btn = document.querySelector('.pizza');
+
+targetDivPizza.style.display = "none";
+
+pizza_btn.onclick = function () {
+    if (targetDivPizza.style.display !== "none") {
+      targetDivPizza.style.display = "none";
+    } else {
+      targetDivPizza.style.display = "block";
+      targetDivPizza.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"}); 
+    }};
+
+
+
+const targetDivDecyzja = document.querySelector('.decyzjaVideo');
+const decyzja_btn = document.querySelector('.decyzja');
+
+targetDivDecyzja.style.display = "none";
+
+decyzja_btn.onclick = function () {
+    if (targetDivDecyzja.style.display !== "none") {
+      targetDivDecyzja.style.display = "none";
+    } else {
+      targetDivDecyzja.style.display = "block";
+      targetDivDecyzja.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"}); 
+    }};
